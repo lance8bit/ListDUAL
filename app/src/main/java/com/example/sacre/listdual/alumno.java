9823f91a2249e9c4ca9a0ee271b3c47943cd38fb
Stephen <ogonzalez@iesjoandaustria.org>
2018-09-25 16:44:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sacre/listdual/alumno.java
@@ -0,0 +1,50 @@
+package com.example.sacre.listdual;
+
+public class alumno {
+
+    private int id_alumno;
+    private String[] asignaturas_alumno;
+    private String nombre_alumno, email_alumno;
+
+    public alumno(int identficador, String[] asignaturas, String nombre, String email ){
+        id_alumno = identficador;
+        asignaturas_alumno = asignaturas;
+        nombre_alumno = nombre;
+        email_alumno = email;
+    }
+
+
+
+    public int getId_alumno() {
+        return id_alumno;
+    }
+
+    public void setId_alumno(int id_alumno) {
+        this.id_alumno = id_alumno;
+    }
+
+    public String getEmail_alumno() {
+        return email_alumno;
+    }
+
+    public void setEmail_alumno(String email_alumno) {
+        this.email_alumno = email_alumno;
+    }
+
+    public String getNombre_alumno() {
+        return nombre_alumno;
+    }
+
+    public void setNombre_alumno(String nombre_alumno) {
+        this.nombre_alumno = nombre_alumno;
+    }
+
+    public String[] getAssgnatiruas_alumno() {
+        return asignaturas_alumno;
+    }
+
+    public void setAssgnatiruas_alumno(String[] assgnatiruas_alumno) {
+        this.asignaturas_alumno = assgnatiruas_alumno;
+    }
+
+}
