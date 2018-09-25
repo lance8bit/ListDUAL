af0b8869be6b8b982343fca809228f94484b14e2
Stephen <ogonzalez@iesjoandaustria.org>
2018-09-25 17:07:06 +0200

    Define inputEditText in Main Activity

--- app/src/main/java/com/example/sacre/listdual/MainActivity.java
+++ app/src/main/java/com/example/sacre/listdual/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sacre.listdual;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -12,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextInputEditText nombre_input = (TextInputEditText) findViewById(R.id.nom_input);
+        TextInputEditText email_input = (TextInputEditText) findViewById(R.id.email_input);
+        TextInputEditText asignaturas_input = (TextInputEditText) findViewById(R.id.asignaturas_input);
+
     }
 
 }
